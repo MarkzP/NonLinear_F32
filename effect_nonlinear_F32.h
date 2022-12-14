6f874281cc68431210b76e4caf042ef81eb8dfba
Marc Paquette <marc.paquette@telus.com>
2022-12-14 16:52:22 -0500

    reworked to give more flexibility

--- effect_nonlinear_F32.h
+++ effect_nonlinear_F32.h
@@ -26,29 +26,114 @@ class AudioEffectNonLinear_F32 : public AudioStream_F32
     AudioEffectNonLinear_F32(void): AudioStream_F32(1, inputQueueArray) {}
     AudioEffectNonLinear_F32(const AudioSettings_F32 &settings): AudioStream_F32(1, inputQueueArray) {}
     
-    virtual void update(void)
+    void gain(float gain = 1.0f)
     {
-      audio_block_f32_t *block = AudioStream_F32::receiveWritable_f32(0);
-      if (!block) return;
-      
-      for (int i = 0; i < block->length; i++)
-      {
-        float x = block->data[i] * aPre;
-        block->data[i] = (aCurve + 1.0f) * x / (1.0f + fabsf(aCurve * x));
-		block->data[i] *= aPost;
-      }
+      __disable_irq();
+      aGain = gain;
+      set_mix();
+      __enable_irq();
+    }    
     
-      AudioStream_F32::transmit(block);
-      AudioStream_F32::release(block);
+    void curve(float curve = 0.0f)
+    {
+      if (curve < 0.0f) curve = 0.0f;
+
+      __disable_irq();
+      aCurve = curve;
+      set_mix();
+      __enable_irq();
+    }
+    
+    void offset(float offset = 0.0f)
+    {
+      aOffset = offset;
     }
 
-    void curve(float curve) { aCurve = curve; }
-    void gain(float pre, float post = 1.0f) { aPre = pre; aPost = post; }
+    void clip(float clip = 1.0f)
+    {
+      if (clip < 0.0f) clip = 0.0f;
+
+      __disable_irq();
+      anClip = -clip;
+      apClip = clip;
+      __enable_irq();
+    }
+    
+    void tone(float tone = 1.0f)
+    {
+      if (tone < 0.0f) tone = 0.0f;
+      else if (tone > 1.0f) tone = 1.0f;
+
+      aLp = 0.1f + (0.9f * (exp2f(tone) - 1.0f));
+    }
+    
+    void level(float level = 1.0f)
+    {
+      if (level < -1.0f) level = -1.0f;
+      else if (level > 1.0f) level = 1.0f;
+      
+      aLevel = level;
+    }
+
+    void set_mix()
+    {
+      aDry = 1.0f / (1.0f + fabsf(aGain) + (aCurve * 1.5f));
+      aWet = 1.0f - aDry;      
+    }
+
+    virtual void update(void)
+    {
+      float sample;
+
+      float gain = aGain;
+      float curve = aCurve;
+      float nclip = anClip;
+      float pclip = apClip;
+      float offset = aOffset;
+      float level = aLevel;
+      float wet = aWet;
+      float dry = aDry;
+      float lp = aLp;
+      float last = aLast;
+
+      audio_block_f32_t *block = AudioStream_F32::receiveWritable_f32(0);
+      if (!block) return;
+
+      for (int i = 0; i < block->length; i++)
+      {
+        sample = block->data[i] * gain;
+        
+        sample = (curve + 1.0f) * sample / (1.0f + fabsf(curve * sample));
+        sample += offset;
+
+        if (sample < nclip) sample = nclip;
+        if (sample > pclip) sample = pclip;
+
+        sample *= level;
+        last += (sample - last) * lp;
+
+        block->data[i] *= dry;
+        block->data[i] += last * wet;
+      }
+
+      AudioStream_F32::transmit(block);
+      AudioStream_F32::release(block);
+
+      aLast = last;
+    }
+
   private:
     audio_block_f32_t *inputQueueArray[1];
+    float aGain = 1.0f;
     float aCurve = 0.0f;
-    float aPre = 1.0f;
-	float aPost = 1.0f;
+    float aOffset = 0.0f;
+    float anClip = -1.0f;
+    float apClip = 1.0f;
+    float aLevel = 1.0f;
+    float aLp = 1.0f;
+    float aLast = 0.0f;
+    float aDry = 1.0f;
+    float aWet = 0.0f;
 };
 
 #endif
