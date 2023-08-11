770ec30125330e4e5585adff51cca1e862e9476b
Marc Paquette <marc.paquette@telus.com>
2023-08-11 18:38:02 -0400

    filter update

--- effect_nonlinear_F32.cpp
+++ effect_nonlinear_F32.cpp
@@ -4,144 +4,171 @@
 
 // The interpolation/decimation filter has been designed to attenuate the upper frequencies way below nyquist.
 // Tuned by ear to offer the best compromise between performance, sound quality & aliasing rejection
-// https://www.arc.id.au/FilterDesign.html lpf 12.1kHz@220.5kHz, 80dB, 55 taps
+// https://www.arc.id.au/FilterDesign.html lpf 13.0kHz@220.5kHz, 86dB, 65 taps
 float FIR_coeffs[INT_NUMTAPS] =
 {
-0.000004, 
-0.000036, 
-0.000117, 
-0.000260, 
-0.000461, 
-0.000684, 
-0.000854, 
-0.000855, 
-0.000545, 
--0.000214, 
--0.001515, 
--0.003351, 
--0.005570, 
--0.007847, 
--0.009672, 
--0.010396, 
--0.009299, 
--0.005704, 
-0.000895, 
-0.010708, 
-0.023551, 
-0.038802, 
-0.055421, 
-0.072046, 
-0.087150, 
-0.099236, 
-0.107049, 
-0.109751, 
-0.107049, 
-0.099236, 
-0.087150, 
-0.072046, 
-0.055421, 
-0.038802, 
-0.023551, 
-0.010708, 
-0.000895, 
--0.005704, 
--0.009299, 
--0.010396, 
--0.009672, 
--0.007847, 
--0.005570, 
--0.003351, 
--0.001515, 
--0.000214, 
-0.000545, 
-0.000855, 
-0.000854, 
-0.000684, 
-0.000461, 
-0.000260, 
-0.000117, 
-0.000036, 
-0.000004
+-0.000009, 
+-0.000032, 
+-0.000072, 
+-0.000123, 
+-0.000167, 
+-0.000172, 
+-0.000095, 
+0.000108, 
+0.000468, 
+0.000981, 
+0.001587, 
+0.002162, 
+0.002521, 
+0.002434, 
+0.001675, 
+0.000079, 
+-0.002387, 
+-0.005565, 
+-0.009059, 
+-0.012241, 
+-0.014290, 
+-0.014294, 
+-0.011384, 
+-0.004897, 
+0.005469, 
+0.019537, 
+0.036598, 
+0.055436, 
+0.074442, 
+0.091806, 
+0.105752, 
+0.114786, 
+0.117914, 
+0.114786, 
+0.105752, 
+0.091806, 
+0.074442, 
+0.055436, 
+0.036598, 
+0.019537, 
+0.005469, 
+-0.004897, 
+-0.011384, 
+-0.014294, 
+-0.014290, 
+-0.012241, 
+-0.009059, 
+-0.005565, 
+-0.002387, 
+0.000079, 
+0.001675, 
+0.002434, 
+0.002521, 
+0.002162, 
+0.001587, 
+0.000981, 
+0.000468, 
+0.000108, 
+-0.000095, 
+-0.000172, 
+-0.000167, 
+-0.000123, 
+-0.000072, 
+-0.000032, 
+-0.000009  
 };
-
 
 bool AudioEffectNonLinear_F32::begin(bool multirate)
 {
-	if (multirate)
-	{
-		arm_status int_init = arm_fir_interpolate_init_f32(&_interpolator, INTERPOLATION, INT_NUMTAPS, FIR_coeffs, _int_state, RAW_BUFFER_SIZE);
-		arm_status dec_init = arm_fir_decimate_init_f32(&_decimator, INT_NUMTAPS, INTERPOLATION, FIR_coeffs, _decim_state, INT_BUFFER_SIZE);
-		_multirate = int_init == ARM_MATH_SUCCESS && dec_init == ARM_MATH_SUCCESS;
-		return _multirate;
-	}
-	
-	_multirate = false;
-	return true;
+  if (multirate)
+  {
+    arm_status int_init = arm_fir_interpolate_init_f32(&_interpolator, INTERPOLATION, INT_NUMTAPS, FIR_coeffs, _int_state, RAW_BUFFER_SIZE);
+    arm_status dec_init = arm_fir_decimate_init_f32(&_decimator, INT_NUMTAPS, INTERPOLATION, FIR_coeffs, _decim_state, INT_BUFFER_SIZE);
+    _multirate = int_init == ARM_MATH_SUCCESS && dec_init == ARM_MATH_SUCCESS;
+    return _multirate;
+  }
+  
+  _multirate = false;
+  return true;
 }
 
-void AudioEffectNonLinear_F32::gain(float gain, float curve)
+void AudioEffectNonLinear_F32::gain(float gain)
 {
-  gain = gain < 0.0f ? 0.0f : gain;
-  curve = curve < 0.0f ? 0.0f : curve > 1.0f ? 1.0f : curve;
+  _gain = gain < 0.0f ? 0.0f : gain;
+}
 
-	float comp = 1.0f + (curve * 0.5f);
-	gain /= comp;
-	curve /= 3.0f;
-  
-	// Compensate interpolation losses
-	if (_multirate) gain *= (float)INTERPOLATION;
+void AudioEffectNonLinear_F32::curve(float curve)
+{
+  _curve = curve < 0.0f ? 0.0f : curve;
+}
 
-	__disable_irq();
-	_gain = gain;
-  _curve = curve;
-  _comp = comp;
-	__enable_irq();   
+void AudioEffectNonLinear_F32::clip(float clip)
+{
+  _clip = clip < 0.0f ? 0.0f : clip > 1.0f ? 1.0f : clip;
+}
+
+void AudioEffectNonLinear_F32::tone(float tone)
+{
+  tone = tone < 0.0f ? 0.0f : tone > 1.0f ? 1.0f : tone;
+  _lpa = (tone * tone * 0.99f) + 0.01f;
 }
 
 void AudioEffectNonLinear_F32::level(float level)
 {
-	_level = level;
+  _level = level < 0.0f ? 0.0f : level;
 }
 
 void AudioEffectNonLinear_F32::update(void)
 {
-	float sample;
-	float gain = _gain;
-  float curve = _curve;
-	float level = _level * _comp;
+  float sample;
 
-	audio_block_f32_t *block = AudioStream_F32::receiveWritable_f32(0);
-	if (!block) return;
-	
-	int len = block->length;
-	float *p = block->data;
-	
-	// Apply gain
-	arm_scale_f32(block->data, gain, block->data, block->length);
-	
-	if (_multirate)
-	{
-		// Interpolate
-		arm_fir_interpolate_f32(&_interpolator, block->data, _interpolated, block->length);
-		len *= INTERPOLATION;
-		p = _interpolated;
-	}
+  audio_block_f32_t *block = AudioStream_F32::receiveWritable_f32(0);
+  if (!block) return;
+
+  int len = block->length;
+  float *p = block->data;
+  
+  if (_multirate)
+  {
+    // Compensate for interpolation losses
+    arm_scale_f32(block->data, (float)INTERPOLATION, block->data, block->length);
+    // Interpolate
+    arm_fir_interpolate_f32(&_interpolator, block->data, _interpolated, block->length);
+    len *= INTERPOLATION;
+    p = _interpolated;
+  }
   
   for (int i = 0; i < len; i++)
   {
     sample = *p;
-    sample = sample < -1.0f ? -1.0f : sample > 1.0f ? 1.0f : sample;
-    sample -= sample * sample * sample * curve;
+
+    // dc block
+    sample -= (_dcpre += (sample - _dcpre) * _dca);
+    
+    // Apply gain
+    sample *= _gain;
+    
+    // Apply curve
+    sample = (_curve + 1.0f) * sample / (1.0f + _curve * fabsf(sample));
+    
+    // Hard clip
+    sample = sample < -_clip ? -_clip : sample > _clip ? _clip : sample;
+    
+    // Apply cubic
+    if (sample > 0.0f) sample -= sample * sample * sample * (1.0f / 3.0f);
+    
+    // 1st order filter to remove harsh harmonics
+    sample = (_lpf += (sample - _lpf) * _lpa);
+    
+    // dc block
+    sample -= (_dcpost += (sample - _dcpost) * _dca);
+    
+    // Apply level
+    sample *= _level;
+   
     *p = sample;
     p++;
   }
 
-	// Decimate
-	if (_multirate) arm_fir_decimate_f32(&_decimator, _interpolated, block->data, len);
+  // Decimate
+  if (_multirate) arm_fir_decimate_f32(&_decimator, _interpolated, block->data, len);
 
-	// Apply level
-	arm_scale_f32(block->data, level, block->data, block->length);
-
-	AudioStream_F32::transmit(block);
-	AudioStream_F32::release(block);
+  AudioStream_F32::transmit(block);
+  AudioStream_F32::release(block);
 }
--- effect_nonlinear_F32.h
+++ effect_nonlinear_F32.h
@@ -15,37 +15,44 @@
 #define INTERPOLATION   5
 #define RAW_BUFFER_SIZE 128
 #define INT_BUFFER_SIZE (RAW_BUFFER_SIZE * INTERPOLATION)
-#define INT_NUMTAPS   55
+#define INT_NUMTAPS   65
 
 class AudioEffectNonLinear_F32 : public AudioStream_F32
 {
 //GUI: inputs:1, outputs:1  //this line used for automatic generation of GUI node
-//GUI: shortName:effect_NonLinear	
-	public:
-		AudioEffectNonLinear_F32(void): AudioStream_F32(1, inputQueueArray) { }
-		AudioEffectNonLinear_F32(const AudioSettings_F32 &settings): AudioStream_F32(1, inputQueueArray) { }
+//GUI: shortName:effect_NonLinear 
+  public:
+    AudioEffectNonLinear_F32(void): AudioStream_F32(1, inputQueueArray) { }
+    AudioEffectNonLinear_F32(const AudioSettings_F32 &settings): AudioStream_F32(1, inputQueueArray) { }
 
-		bool begin(bool multirate = true);
+    bool begin(bool multirate = true);
     
-		void gain(float gain = 1.0f, float curve = 0.0f);
-		
-		void level(float level = 1.0f);
-		
-		virtual void update(void);
+    void gain(float gain = 0.1f);
+    void curve(float curve = 0.0f);
+    void clip(float clip = 1.0f);
+    void tone(float tone = 1.0f);
+    void level(float level = 10.0f);
+    
+    virtual void update(void);
 
-	private:
-		audio_block_f32_t *inputQueueArray[1];
-		float _gain = 1.0f;
+  private:
+    audio_block_f32_t *inputQueueArray[1];
+    float _gain = 1.0f;
     float _curve = 0.0f;
-		float _level = 1.0f;
-    float _comp = 1.0f;
-	
-		arm_fir_interpolate_instance_f32 _interpolator;
-		arm_fir_decimate_instance_f32 _decimator;
-		float _interpolated[INT_BUFFER_SIZE];
-		float _int_state[(INT_NUMTAPS / INTERPOLATION) + RAW_BUFFER_SIZE - 1];
-		float _decim_state[INT_NUMTAPS + INT_BUFFER_SIZE - 1];
-		bool _multirate = false;
+    float _clip = 1.0f;
+    float _level = 1.0f;
+    float _lpa = 1.0f;
+    float _lpf = 0.0f;
+    const float _dca = 0.0005f;
+    float _dcpre = 0.0f;
+    float _dcpost = 0.0f;
+  
+    arm_fir_interpolate_instance_f32 _interpolator;
+    arm_fir_decimate_instance_f32 _decimator;
+    float _interpolated[INT_BUFFER_SIZE];
+    float _int_state[(INT_NUMTAPS / INTERPOLATION) + RAW_BUFFER_SIZE - 1];
+    float _decim_state[INT_NUMTAPS + INT_BUFFER_SIZE - 1];
+    bool _multirate = false;
 };
 
 #endif
